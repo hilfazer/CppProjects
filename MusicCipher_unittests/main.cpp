b267d37fa51f1b541c1a07f09d93cb10f1b2c12a
hilfazer <az13337@gmail.com>
2025-06-24 11:46:54 +0200

    MusicCipherUnitTests

---
+++ MusicCipher_unittests/main.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE MusicCipherTestModule
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(
+	MusicCipherSuite)
+
+BOOST_AUTO_TEST_CASE(
+	MusicCipherUnitTests)
+{
+	BOOST_TEST(true /* test assertion */);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
