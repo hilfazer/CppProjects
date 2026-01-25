33b2cdc5779e106e3a321e14a036e6b765dcd54e
hilfazer <az13337@gmail.com>
2026-01-25 20:16:53 +0100

    mismatch

--- ceePlusPlus/main.cpp
+++ ceePlusPlus/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 
-using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	std::vector<std::string> zxc = {"foo", "baz"};
+	std::vector<std::string> asd = {"foo", "bar", "baz"};
+
+	auto mismatch = std::mismatch(zxc.begin(), zxc.end(), asd.begin(), asd.end());
+
+	if( mismatch.first != zxc.end() )
+		std::cout << "first: " << *mismatch.first << std::endl;
+
+	if( mismatch.second != asd.end() )
+		std::cout << "second: " << *mismatch.second << std::endl;
 	return 0;
 }
