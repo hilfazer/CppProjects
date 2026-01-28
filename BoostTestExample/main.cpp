66f15035b19b2148d9812c859685f12c7b8b16e7
hilfazer <az13337@gmail.com>
2026-01-28 17:20:38 +0100

    added suiteTwo.cpp

--- BoostTestExample/main.cpp
+++ BoostTestExample/main.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
+#define BOOST_TEST_MODULE My test module
+#include <boost/test/included/unit_test.hpp>
 
-using namespace std;
-
-int main()
+int add(int a, int b)
 {
-	cout << "Hello World!" << endl;
-	return 0;
+	return a + b;
 }
+
+BOOST_AUTO_TEST_SUITE(SuiteExample)
+
+BOOST_AUTO_TEST_CASE(CaseExample)
+{
+	BOOST_TEST(true /* test assertion */);
+}
+
+BOOST_AUTO_TEST_CASE(TestAdd)
+{
+	BOOST_TEST(add(2,3) == 5);
+}
+
+BOOST_AUTO_TEST_CASE(TestAddFail)
+{
+	BOOST_TEST(add(2,3) == 3);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ BoostTestExample/suiteTwo.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(SuiteTwo)
+
+BOOST_AUTO_TEST_CASE(CaseOne)
+{
+	BOOST_TEST(true /* test assertion */);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
