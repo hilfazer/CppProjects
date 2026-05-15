778b76d92d704a220deb86dd2dce6f1f29a3eb60
hilfazer <az13337@gmail.com>
2026-05-15 16:13:43 +0200

    methods/fields in ScribbleArea.hpp

--- Paint/Paint/ScribbleArea.hpp
+++ Paint/Paint/ScribbleArea.hpp
@@ -11,10 +11,34 @@ class ScribbleArea : public QWidget
 
 public:
 	ScribbleArea(QWidget* parent = nullptr);
+	bool openImage(const QString& fileName);
+	bool saveImage(const QString& fileName, const char* fileFormat);
+	void setPenColor(const QColor& newColor);
+	void setPenWidth(int newWidth);
+
+	bool isModified() const { return modified; }
+	QColor penColor() const { return myPenColor; }
+	int penWidth() const { return myPenWidth; }
 
 public slots:
+	void clearImage();
+	void print();
 
 protected:
+	void mousePressEvent(QMouseEvent* event) override;
+	void mouseMoveEvent(QMouseEvent* event) override;
+	void mouseReleaseEvent(QMouseEvent* event) override;
+	void paintEvent(QPaintEvent* event) override;
+	void resizeEvent(QResizeEvent* event) override;
+
 
 private:
+	void drawLineTo(const QPoint& endPoint);
+	void resizeImage(QImage* image, const QSize& newSize);
+	bool modified;
+	bool scribbling;
+	int myPenWidth;
+	QColor myPenColor;
+	QImage image;
+	QPoint lastPoint;
 }
